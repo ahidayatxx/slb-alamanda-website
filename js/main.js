4186b4c9acbc9dcd82d24c76276be4f533a09b93
ahidayatxx <161401009+ahidayatxx@users.noreply.github.com>
2025-07-20 14:21:38 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// main.js
+// File ini disiapkan untuk fungsionalitas interaktif di masa depan.
+// Untuk saat ini, tidak ada JavaScript kustom yang diperlukan untuk menjaga situs tetap ringan dan cepat.
